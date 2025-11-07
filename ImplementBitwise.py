66d75d4b398da4007555c66c777f401dc8a1778e
zarifkhan-07 <zarifsuhana1@gmail.com>
2025-11-07 18:27:01 +0600

    Add files via upload

---
+++ ImplementBitwise.py
@@ -0,0 +1,10 @@
+n1=10
+n2=4
+
+print("n1 & n2 are:",n1,n2)
+print("n1 | n2 are:", n1|n2)
+print("n1^n2 are:",n1^n2)
+print("n1<<n2 are:",n1<<n2)
+print("n1>>n2 are:",n1>>n2)
+print("-n1 are:",-n1)
+print("-n2 are:",-n2)
